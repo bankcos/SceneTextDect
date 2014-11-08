23588aa0728e74c7e25a55e09a7f19de524b1fe9
bankcos <bankcos@163.com>
2014-11-08 08:57:38 +0800

    v0.25
    完成特征点提取，完成熵计算，可以剔除关键点

--- cpp/SIFT.cpp
+++ cpp/SIFT.cpp
@@ -16,11 +16,16 @@ float entropy(Mat Roi, KeyPoint p)
 	float hui[256] = { 0 };
 	for (i = p.pt.x - p.size; i < p.pt.x + p.size; i++)
 	{
+		//边界检查
+		if (i < 0) i = 0;
+		if (i>img_1.rows - 1) continue;
 		for (j = p.pt.y - p.size;j<p.pt.y+p.size; j++)
 		{
-			hui[img_1.at<uchar>(i, j)]++;
-			//			cout << a++ << endl;
-			//img_1.at<uchar>(i, j)[0] = 0;
+			if (j < 0) j = 0;
+			if (j>img_1.cols - 1) {
+				continue;
+			}
+			else hui[img_1.at<uchar>(i, j)]++;
 		}
 	}
 	for (i = 0; i < 256; i++){
@@ -30,21 +35,16 @@ float entropy(Mat Roi, KeyPoint p)
 	for (i = 0; i < 256; i++){
 		if (hui[i] != 0){
 			result = result - hui[i] * log2(hui[i]);
-
 		}
 	}
-
-
 	return result;
 }
 
 int main(int argc, char** argv)
 {
-	Mat M(7, 7, CV_32FC2, Scalar(1, 3));
-	Mat img_1 = imread("d:\\project\\smallgray.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Mat img_1 = imread("d:\\project\\gray.jpg",CV_LOAD_IMAGE_GRAYSCALE);
 	if (!img_1.data){
 		std::cout << "Can't open" << std::endl;
-
 	}
 	SiftFeatureDetector detector;
 	std::vector<KeyPoint> kp1, kp2;
@@ -52,19 +52,29 @@ int main(int argc, char** argv)
 	//FeaturesExtract
 	SiftDescriptorExtractor extractor;
 	Mat des1;
+	//提取关键点
 	extractor.compute(img_1,kp1,des1);
-
+	//res为保存画点后的图形
 	Mat res1;
-	int drawmode = DrawMatchesFlags::DRAW_RICH_KEYPOINTS;
-	drawKeypoints(img_1, kp1, res1, Scalar::all(-1), 4);
+	
 	std::cout << "size of description of Img1: " << kp1.size() << std::endl;
 	
+
+	int numOfkp=0;//关键点数量
+	float sum,perEntropy;
+	sum = 0;
+	for (numOfkp = 0; numOfkp < kp1.size(); numOfkp++){
+		perEntropy=entropy(img_1, kp1[numOfkp]);
+		if (perEntropy < 0.1) kp1.pop_back();
+		cout << numOfkp << "  " << perEntropy << endl;
+	}
+
+	drawKeypoints(img_1, kp1, res1, Scalar::all(-1));
 	IplImage* transimg1 = cvCloneImage(&(IplImage)res1);
-//	namedWindow("Dis", WINDOW_AUTOSIZE);
+
+	namedWindow("Dis", WINDOW_AUTOSIZE);
 	cvShowImage("Dis", transimg1);
-//	imshow("Dis", img_1	);
-	cout << entropy(img_1,kp1[9]);
-//	entropy(des1, img_1.size(), 256);
+
 	waitKey(0);
 	return 0;
 }
