b8a6ab095bc25897464a5ff94fa0dd133afb01b0
bankcos <bankcos@163.com>
2014-11-14 14:09:31 +0800

    v0.36
    增加文件读取

---
+++ cpp/file.cpp
@@ -0,0 +1,28 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main()
+{
+
+	string samplePath = "d:\\project\\MSRG\\MSRG_";
+	string path;
+	char sampleName[256];
+	for (int i = 1; i < 200; i++)
+	{
+		memset(sampleName, '\0', sizeof(char)* 256);
+		sprintf(sampleName, "%04i.jpg", i);
+		path = samplePath + sampleName;
+		cout << path << endl;
+	}
+
+
+	system("pause");
+}
